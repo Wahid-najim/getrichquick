8674fb9b7793ee6ee05a89fb17105e17110a6183
Wahid Najim <133244826+Wahid-najim@users.noreply.github.com>
2023-12-30 22:45:47 +0600

    getrichquick.sh

--- getrichquick.sh
+++ getrichquick.sh
@@ -1,1 +1,24 @@
 
+#!/bin/bash
+echo "welcome here "
+
+echo "what is your name?"
+read name
+sleep 2
+echo "How old are you?"
+read age
+
+sleep 2
+echo " hello $name , your age is $age"
+
+echo "Calculating"
+sleep 4
+echo "................."
+echo " *****************....."
+
+sleep 2
+
+getrich=$((( $RANDOM % 15) + $age))
+
+echo "$name , you will become rich  when you are $getrich old."
+
